e97a6d3a2d982b031627946e1bb5cc8716e2ae41
曾柏璁 <tsungtseng@secom.com.tw>
2024-03-19 17:23:34 +0800

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { defineAsyncComponent } from 'vue'
+const CSSRuntimeProvider = defineAsyncComponent(async () => (await import('@master/css.vue')).CSSRuntimeProvider)
+</script>
+<template>
+  <CSSRuntimeProvider :config="import('./master.css')">
+    <div class="light">
+    </div>
+  </CSSRuntimeProvider>
+</template>
