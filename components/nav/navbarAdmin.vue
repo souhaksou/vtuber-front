f0858739ca7dab530ddd2b95910f0784ba12794a
souhaksou <98225683+souhaksou@users.noreply.github.com>
2024-04-03 00:18:52 +0800

    20240403 fix routes

--- components/nav/navbarAdmin.vue
+++ components/nav/navbarAdmin.vue
@@ -1,8 +1,10 @@
 <template>
   <nav class="bg:#EE9FCB flex jc:space-between ai:center p:12|32">
-    <div class="flex jc:space-between ai:center {fg:#F8F8FF;f:18;text-shadow:1|2|3|#653951;inline-block;p:8|16;bb:2|solid|#EE9FCB;transition:400ms;r:4;}>a 
-    {border-color:#F8F8FF;}>a:hover {bg:#E15EA7;}>a.router-link-exact-active">
-      <NuxtLink to="/admin"> <i class="bi bi-house-door-fill mr:8"></i>首頁</NuxtLink>
+    <div
+      class="flex jc:space-between ai:center {fg:#F8F8FF;f:18;text-shadow:1|2|3|#653951;inline-block;p:8|16;bb:2|solid|#EE9FCB;transition:400ms;r:4;}>a">
+      <NuxtLink to="/admin/home"> <i class="bi bi-house-door-fill mr:8"></i>首頁</NuxtLink>
+      <div class="w:8"></div>
+      <NuxtLink to="/admin/featured"><i class="bi bi-bookmark-star-fill mr:8"></i>精選</NuxtLink>
       <div class="w:8"></div>
       <NuxtLink to="/admin/articles"><i class="bi bi-book-half mr:8"></i>文章</NuxtLink>
       <div class="w:8"></div>
@@ -13,7 +15,7 @@
       <NuxtLink to="/admin/charts"><i class="bi bi-bar-chart-fill mr:8"></i>圖表</NuxtLink>
     </div>
     <div class="flex ai:center">
-      <NuxtLink to="/admin/admin" class="fg:#F8F8FF f:18 text-shadow:1|2|3|#653951 inline-block p:8|16">
+      <NuxtLink to="/admin" class="fg:#F8F8FF f:18 text-shadow:1|2|3|#653951 inline-block p:8|16">
         <i class="bi bi-person-circle mr:8"></i>管理
       </NuxtLink>
       <div class="w:8"></div>
---
+++ pages/admin/featured.vue
@@ -0,0 +1,10 @@
+<script setup>
+definePageMeta({
+  layout: 'admin'
+})
+</script>
+<template>
+  <div>
+    <p>文章精選</p>
+  </div>
+</template>
